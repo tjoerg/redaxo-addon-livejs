0e04dfeb6b1f521554a92ce069744689a007d305
tjoerg <joerschi92@web.de>
2014-11-19 14:24:07 +0100

    Update install.inc.php

--- install.inc.php
+++ install.inc.php
@@ -4,9 +4,7 @@
 	if ($error != '') {
 	    $REX['ADDON']['installmsg']['livejs'] = $error;
 	    $REX['ADDON']['install']['livejs'] = false;
-	    echo '(Report.Nr: #002)';
 	} else {
-		echo '(Report.Nr: #001)';
 	    $REX['ADDON']['install']['livejs'] = true;
 	}
 	
